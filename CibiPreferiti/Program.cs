3a9f77eb36c220c2a32c37010a8102029b508b3c
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-04-01 16:26:21 +0200

    last element

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -7,12 +7,18 @@ namespace CibiPreferiti
         static void Main(string[] args)
         {
             string[] cibiPreferiti = { "lasagne", "parmigiana", "pasta norma", "bistecca", "cotolette" };
-            Console.WriteLine(cibiPreferiti.Length);
+            Console.WriteLine("Lunghezza array: " + cibiPreferiti.Length);
 
             for (int i = 0; i < cibiPreferiti.Length; i++)
             {
                 Console.WriteLine(cibiPreferiti[i] + " {0}° classificato",i+1);
             }
+
+            Console.WriteLine(cibiPreferiti[0] + "è il tuo cibo Top preferito");
+
+            Console.WriteLine(cibiPreferiti[cibiPreferiti.Length-1] + "è il tuo ultimo cibo preferito");
+
+
 
         }
     }
