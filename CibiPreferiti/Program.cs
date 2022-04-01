0f09ba4e77b712d14648f7f0959e7fc485f01ba8
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-04-01 16:41:36 +0200

    esercizio finito

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -6,7 +6,7 @@ namespace CibiPreferiti
     {
         static void Main(string[] args)
         {
-            string[] cibiPreferiti = { "lasagne", "parmigiana", "pasta norma", "bistecca", "cotolette" };
+            string[] cibiPreferiti = { "lasagne", "parmigiana", "pasta norma", "bistecca", "cotolette", "orata" };
             Console.WriteLine("Lunghezza array: " + cibiPreferiti.Length);
 
             for (int i = 0; i < cibiPreferiti.Length; i++)
@@ -14,11 +14,17 @@ namespace CibiPreferiti
                 Console.WriteLine(cibiPreferiti[i] + " {0}° classificato",i+1);
             }
 
-            Console.WriteLine(cibiPreferiti[0] + "è il tuo cibo Top preferito");
+            Console.WriteLine(cibiPreferiti[0] + " è il tuo cibo Top preferito");
 
-            Console.WriteLine(cibiPreferiti[cibiPreferiti.Length-1] + "è il tuo ultimo cibo preferito");
+            Console.WriteLine(cibiPreferiti[cibiPreferiti.Length-1] + " è il tuo ultimo cibo preferito");
 
-
+            if (cibiPreferiti.Length  %2 == 0)
+            {
+                Console.WriteLine(cibiPreferiti[cibiPreferiti.Length / 2] + " e " + cibiPreferiti[cibiPreferiti.Length / 2 - 1] + " Sono i tuoi cibi mediani preferiti");
+            }  else
+            {
+                Console.WriteLine(cibiPreferiti[cibiPreferiti.Length / 2] + " è il tuo cibo mediano preferito");
+            }
 
         }
     }
